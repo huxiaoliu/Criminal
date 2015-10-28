523c03f9ffbbaa16a274115169b67617b31aa0e3
huxiaoliu <huxiaoxiaoliu@163.com>
2015-10-28 22:57:00 +0800

    稍微丰富了一下布局页面

--- app/src/main/java/criminal/criminal/activity/CrimeActivity.java
+++ app/src/main/java/criminal/criminal/activity/CrimeActivity.java
@@ -1,7 +1,6 @@
 package criminal.criminal.activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
@@ -15,8 +14,8 @@ import criminal.criminal.fragment.CrimeFragment;
 public class CrimeActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
 
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
--- app/src/main/java/criminal/criminal/model/Crime.java
+++ app/src/main/java/criminal/criminal/model/Crime.java
@@ -1,5 +1,6 @@
 package criminal.criminal.model;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -11,6 +12,31 @@ public class Crime {
     private UUID mId;   //ID
 
     private String mTitle;  //标题
+
+    private Date mDate; //时间
+
+    private boolean mSolved;    //状态
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 
     public UUID getId() {
         return mId;
